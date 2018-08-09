e049f6a0f8d105ee9e554c3d903b729e44d94f06
Ankita Gupta <ankitagupta12391@gmail.com>
2018-08-09 14:24:51 +0800

    Add launch config and ASG

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "my-bucket-name"
+#     key = "filename.tfstate"
+#     region = "eu-west-2"
+#     encrypt = true
+#   }
+# }
+
 provider "aws" {
   region = "${var.aws_region}"
   profile = "${var.aws_profile}"
@@ -64,3 +73,112 @@ resource "aws_elb" "ecs-jugnuu-load-balancer" {
     Name = "ecs-jugnuu-load-balancer"
   }
 }
+
+# Create AWS instance role to allow EC2 instances to communicate with ECS
+resource "aws_iam_role" "ecs-instance-role" {
+  name = "ecs_instance_role"
+  description = "Allows EC2 instances to communicate with ECS and read from S3"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "ec2-instance-profile" {
+  name = "ec2-instance-profile"
+  role = "${aws_iam_role.ecs-instance-role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-instance-policy-1" {
+  role       = "${aws_iam_role.ecs-instance-role.name}"
+  policy_arn = "${var.aws_s3_read_policy_arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-instance-policy-2" {
+  role       = "${aws_iam_role.ecs-instance-role.name}"
+  policy_arn = "${var.aws_ec2_instance_policy_arn}"
+}
+
+# Create AWS ECS service role ro allow ECS cluster to communicate with ELB
+resource "aws_iam_role" "ecs-service-role" {
+  name = "ecs_service_role"
+  description = "Allows ECS cluster to communicate with ELB"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-service-policy-1" {
+  role       = "${aws_iam_role.ecs-service-role.name}"
+  policy_arn = "${var.aws_ec2_service_policy_arn}"
+}
+
+# Create auto-scaling group
+
+# User data
+
+data "aws_ami" "linux-ecs-optimized" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-2018.03.a-amazon-ecs-optimized"]
+  }
+
+  owners = ["591542846629"] # Canonical
+}
+
+resource "aws_launch_configuration" "aws-launch-config" {
+  name_prefix   = "jugnuu_launch_config"
+  image_id      = "${data.aws_ami.linux-ecs-optimized.id}"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.ecs-jugnuu-security-group.id}"]
+  key_name = "${var.key_name}"
+  iam_instance_profile = "${aws_iam_instance_profile.ec2-instance-profile.name}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  user_data = <<EOF
+#!/bin/bash
+yum install -y aws-cli
+aws s3 cp s3://jugnuu-ecs-config/ecs.config /etc/ecs/ecs.config
+EOF
+}
+
+resource "aws_autoscaling_group" "aws-auto-scaling-group" {
+  name                 = "jugnuu-asg"
+  launch_configuration = "${aws_launch_configuration.aws-launch-config.name}"
+  vpc_zone_identifier  = ["${data.aws_subnet_ids.default-subnet-ids.ids}"]
+  min_size             = 2
+  max_size             = 2
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
